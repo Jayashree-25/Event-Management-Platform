13ca8551ccff4beb226a72caed83b10e76dd7226
jayashre-25 <dasjayashree8260@gmail.com>
2025-03-02 14:16:13 +0530

    1 more added

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+const isLoggedIn = false;
+
+
 window.addEventListener("load", function () {
     const navEntries = performance.getEntriesByType("navigation");
     const navType = navEntries.length > 0 ? navEntries[0].type : "navigate";
-
     if (navType === "back_forward") {
-       
         document.getElementById("intro-screen").style.display = "none";
         document.getElementById("main-content").classList.remove("hidden");
     } else {
-       
         setTimeout(() => {
             const introScreen = document.getElementById("intro-screen");
             introScreen.classList.add("fade-out");
@@ -17,11 +17,17 @@ window.addEventListener("load", function () {
             }, 1000);
         }, 4000);
     }
+
+
+    if (isLoggedIn) {
+        document.getElementById("profile-icon").classList.remove("hidden");
+        document.getElementById("section").classList.add("hidden");
+    }
 });
 
 
-document.getElementById("search-btn").addEventListener("click", function () {
-    let query = document.getElementById("search").value.trim();
+document.getElementById("search-btn")?.addEventListener("click", function () {
+    const query = document.getElementById("search").value.trim();
     if (query) {
         alert("You searched for: " + query);
     } else {
